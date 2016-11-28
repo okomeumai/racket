16720997a3d2bf87b073e5e28d9f7d6cebc53ce4
okomeumai <highberg525@gmail.com>
2016-11-28 22:03:54 +0900

    購入画面に「トップに戻る」設置

--- resources/views/finish.blade.php
+++ resources/views/finish.blade.php
@@ -5,7 +5,7 @@
 @section('content')
 
 <div class="content">
-        <div class="finish-msg">ご購入ありがとうございました。</div>
+        <div class="finish-msg">ご購入完了致しました。</div>
         <div class="cart-list-title">
             <span class="cart-list-price">価格</span>
             <span class="cart-list-num">数量</span>
@@ -15,9 +15,9 @@
                 <li>
                     <div class="cart-item">
                         <img class="cart-item-img" src="/{{ \Config::get('const.ITEM_IMG_PATH') }}/{{ $cart->item->img }}">
-                        <span class="cart-item-name">{{ $cart->item->name }}</span>
-                        <span class="cart-item-price">¥ {{ $cart->item->price }}</span>
-                        <span class="finish-item-price">{{ $cart->amount }}</span>
+                        <span class="cart-item-name">商品名：{{ $cart->item->name }}</span>
+                        <span class="cart-item-price">価格：{{ $cart->item->price }}円</span>
+                        <span class="finish-item-price">数量：{{ $cart->amount }}</span>
                     </div>
                 </li>
                 @endforeach
@@ -25,6 +25,8 @@
         <div class="buy-sum-box">
             <span class="buy-sum-title">合計</span>
             <span class="buy-sum-price">¥{{ $priceSum }} </span>
+            <span class="finish-msg">お買い上げありがとうございました。</span>
+            <a href="/">トップページに戻る</a>
         </div>
     </div>
 

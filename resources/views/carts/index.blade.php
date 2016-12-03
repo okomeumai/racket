c7fe9af91144102f23315ca532990f92e0a850e6
okomeumai <highberg525@gmail.com>
2016-12-03 19:00:15 +0900

    カテゴリー別商品一覧のHTMLとCSSおよびカートページのHTMLとCSSを追記

--- resources/views/carts/index.blade.php
+++ resources/views/carts/index.blade.php
@@ -1,51 +1,72 @@
-@extends('layouts.default')
-@section('title', 'カート')
-@section('content')
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="utf-8">
+    <title>カート</title>
+  <link rel="stylesheet" href="/css/cart.css">
+</head>
+<body>
+  <div id="container">
+    <header>
+      <div class="img_title">
+        <a href="/"><img src="/images/title.png"></a>
+        <p>〜プレイスタイル別テニスラケットSHOP〜</p>
+      </div>
+      <div class="buy">
+        @if ( $priceSum )
+        <form action="{{ url('/carts/buy') }}" method="post">
+          {{ csrf_field() }}
+        <a href="/carts/buy"><p>購入する</p></a>
+        </form>
+        @endif
+      </div> <!--buy閉じ-->
+    </header>
 
-<div class="container">
-  <h1>ショッピングカート</h1>
+    <div class="text_cart">
+      <h1>ショッピングカート</h1>
+    </div>
 
-@if(!empty($carts))
-  <div class="cart-list-title">
-    <span class="cart-list-price">価格</span>
-    <span class="cart-list-num">数量</span>
-  </div>
-@endif
+    <!--左サイドのカテゴリー-->
+    <nav>
+      <div class="nav">
+        <div class="nav_ttl">
+          <p>スタイル一覧</p>
+        </div> <!--nav_ttl閉じ-->
+        <ul>
+          <li><a href="/items/オールラウンダー" target="_blank">オールラウンダー</a></li>
+          <li><a href="/items/サーブ＆ボレーヤー" target="_blank">サーブ＆ボレーヤー</a></li>
+          <li><a href="/items/ベースライナー" target="_blank">ベースライナー</a></li>
+        </ul>
+      </div><!--nav閉じ-->
+    </nav>
 
-  <ul class="cart-list">
-  @foreach($carts as $cart)
-    <li>
-      <div class="cart-item">
-        <img class="cart-item-img" src="/{{ \Config::get('const.ITEM_IMG_PATH') }}/{{ $cart->item->img }}">
-        <span class="cart-item-name">{{ $cart->item->name }} </span>
-        <form class="cart-item-del" action="{{ url('/cart', $cart->id) }}"  method="post">
+    @foreach($carts as $cart)
+      <div class="item_of_cart">
+        <img src="/{{ \Config::get('const.ITEM_IMG_PATH') }}/{{ $cart->item->img }}">
+        <p>商品名：{{ $cart->item->name }} </p>
+        <p>価格：{{ $cart->item->price }}円 </p>
+        <form  action="{{ url('/cart', $cart->id) }}" method="post">
+          {{ csrf_field() }}
+          {{ method_field('patch') }}
+          <input type="number" type="text" name="amount" value={{ $cart->amount }}>個&nbsp;&nbsp;<input type="submit" value="変更する">
+        </form>
+        <form action="{{ url('/cart', $cart->id) }}"  method="post">
           {{ csrf_field() }}
           {{ method_field('delete') }}
           <input type="submit" value="削除">
         </form>
-        <span class="cart-item-price">¥ {{ $cart->item->price }}</span>
-        <form class="form_select_amount" id="form_select_amount1" action="{{ url('/cart', $cart->id) }}" method="post">
-          {{ csrf_field() }}
-          {{ method_field('patch') }}
-          <input type="number" type="text" class="input_text_width text_align_right" name="amount" value={{ $cart->amount }}>個&nbsp;&nbsp;<input type="submit" value="変更する">
-        </form>
-      </div> <!--cart-item閉じ-->
-    </li>
-  @endforeach
-  </ul>
-
-  <div class="buy-sum-box">
-    <span class="buy-sum-title">合計</span>
-    <span class="buy-sum-price">¥{{ $priceSum }} </span>
-  </div> <!--buy-sum-box閉じ-->
-
-      <div>
-        @if ( $priceSum )
-        <form action="{{ url('/carts/buy') }}" method="post">
-          {{ csrf_field() }}
-          <input class="buy-btn" type="submit" value="購入する">
-        </form>
-        @endif
-      </div>
+      </div> <!--item_of_cart閉じ-->
+    @endforeach
+    <div class="buy-sum-box">
+      <span class="buy-sum-title">合計</span>
+      <span class="buy-sum-price">{{ $priceSum }}円</span>
+      @if ( $priceSum )
+      <form action="{{ url('/carts/buy') }}" method="post">
+        {{ csrf_field() }}
+        <input class="buy-btn" type="submit" value="購入する">
+      </form>
+      @endif
+    </div> <!--buy-sum-box閉じ-->
 </div> <!--container閉じ-->
-@endsection
+</body>
+</html>
--- resources/views/items/index.blade.php
+++ resources/views/items/index.blade.php
@@ -11,21 +11,37 @@
 <body>
   <div id="container">
     <header>
-      <a href="/"><img src="/images/title.png"></a>
-      <p>〜プレイスタイル別テニスラケットSHOP〜</p>
+      <div class="img_title">
+        <a href="/"><img src="/images/title.png"></a>
+        <p>〜プレイスタイル別テニスラケットSHOP〜</p>
+      </div> <!--img_title閉じ-->
+      <div class="watchCart">
+        <a href="/carts"><p>カートの中身を見る</p></a>
+      </div> <!--watchCart閉じ-->
     </header>
 
-  <a href="/carts">カートの中身を見る</a>
-
     <!--各カテゴリー向け一覧-->
-    <div>
+    <div class="text_category">
       <h1>{{ $item->category }}向け商品一覧</h1>
     </div>
 
     <!--各カテゴリー別説明文掲載-->
-    <div class="text_category">
-
+    <div>
     </div>
+
+    <!--左サイドのカテゴリー-->
+    <nav>
+      <div class="nav">
+        <div class="nav_ttl">
+          <p>スタイル一覧</p>
+        </div> <!--nav_ttl閉じ-->
+        <ul>
+          <li><a href="/items/オールラウンダー" target="_blank">オールラウンダー</a></li>
+          <li><a href="/items/サーブ＆ボレーヤー" target="_blank">サーブ＆ボレーヤー</a></li>
+          <li><a href="/items/ベースライナー" target="_blank">ベースライナー</a></li>
+        </ul>
+      </div><!--nav閉じ-->
+    </nav>
 
     <!--商品列挙-->
     @foreach($items as $item)
@@ -33,14 +49,18 @@
         <img src="/itemimages/{{ $item->img }}" height="130">
         <p class="items_name">商品名：{{ $item->name }}</p>
         <p class="items_price">価格：{{ $item->price }}円</p>
-        <p class="items_amount">数量：{{ $item->amount }}</p>
-      </div> <!--items_detail閉じ-->
-
+        <p class="items_amount">数量：
+          <select name="items_amount">
+            <option value="">１</option><option value="2">２</option><option value="3">３</option><option value="4">４</option><option value="5">５</option>
+            <option value="6">６</option><option value="7">７</option><option value="8">８</option><option value="9">９</option><option value="10">１０</option>
+          </select>
+        </p>
       <!--「カートに入れるボタン」実装-->
       <form action="{{ action('CartsController@create',[$category, $item->id]) }}" id="form_{{ $item->id }}" method="post" style="display:inline">
         {{ csrf_field() }}
-        <input type="submit" class="fs12" value="カートに入れる">
+        <button>カートに入れる</button>
       </form>
+      </div> <!--items_detail閉じ-->
     @endforeach
   </div> <!--container閉じ-->
 </body>
